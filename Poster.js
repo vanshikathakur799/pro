4b2c6db75057f724c39585f1ff10a817288bc8f0
Vanshika Thakur <103041940+vanshikathakur799@users.noreply.github.com>
2024-04-28 17:23:36 +0530

    Add files via upload

---
+++ Poster.js
@@ -0,0 +1,54 @@
+AFRAME.registerComponent('comics-posters', {
+  init: function() {
+    this.postersContainer = this.el
+    this.posters()
+  },
+
+  posters: function() {
+    const postersRef = [
+      {id: 'captainAmerica', url: 'assets/Captain-America.jpg'},
+      {id: 'spiderMan', url: './assets/Spider-Man.jpg'},
+      {id: 'ironMan', url: './assets/Iron-Man.jpg'},
+      {id: 'batMan',url: './assets/Bat-Man.jpg'},
+      {id: 'superMan', url: './assets/Super-Man.jpg'},
+      {id: 'venom', url: './assets/venom.jpg'}
+    ]
+    let prevoiusXPosition = -85
+
+    for (var item of postersRef) {
+      const posX = prevoiusXPosition + 25
+      const posY = 10
+      const posZ = -40
+      const position = { x: posX, y: posY, z: posZ }
+      prevoiusXPosition = posX
+
+      const borderEl = this.createBorder(position, item.id)
+
+      const poster = this.createPoster(item)
+      borderEl.appendChild(poster)
+
+      this.postersContainer.appendChild(borderEl)
+    }
+  },
+
+  createBorder: function(position, id) {
+    const entityEl = document.createElement('a-entity')
+    entityEl.setAttribute('id', id)
+    entityEl.setAttribute('visible', true)
+    entityEl.setAttribute('geometry', {primitive: 'plane', width: 22, height: 40})
+    entityEl.setAttribute('position', position)
+    entityEl.setAttribute('material', { color: '#fff' })
+
+    return entityEl
+  },
+
+  createPoster: function(item) {
+    const entityEl = document.createElement('a-entity')
+    entityEl.setAttribute('visible', true)
+    entityEl.setAttribute('geometry', {primitive: 'plane', width: 20, height: 28})
+    entityEl.setAttribute('position', { x: 0, y: 5, z: 0.1 })
+    entityEl.setAttribute('material', { src: item.url })
+
+    return entityEl
+  }
+})
